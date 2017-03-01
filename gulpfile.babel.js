daa08bc9f75a3a3c945cd186bc34b2ffce705dc7
Anjou Stiles <anjou.stiles@gmail.com>
2017-02-28 22:53:01 -0600

    Rename to ANSImilate

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -133,7 +133,7 @@ gulp.task('wiredep', () => {
 gulp.task('package', function () {
   var manifest = require('./dist/manifest.json');
   return gulp.src('dist/**')
-      .pipe($.zip('ansi up chrome-' + manifest.version + '.zip'))
+      .pipe($.zip('ansimilate-' + manifest.version + '.zip'))
       .pipe(gulp.dest('package'));
 });
 
